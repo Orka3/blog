4bca7d3d7e414bd48c6767b0c4cfcaf7b4e17c3a
thddh <dhthdrud333>
2022-04-01 20:22:28 +0900

    img

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/blog/'
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'orka',
